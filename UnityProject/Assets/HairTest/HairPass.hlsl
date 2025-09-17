05ab42f90c48a3829254609c7d63019249594288
xuanshushu <910071006@qq.com>
2025-09-17 23:11:19 +0800

    2.0.0 prepare

---
+++ UnityProject/Assets/HairTest/HairPass.hlsl
@@ -0,0 +1,347 @@
+﻿#ifndef HART_PASS_INCLUDED
+#define HART_PASS_INCLUDED
+     #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/SurfaceInput.hlsl"
+
+            CBUFFER_START(UnityPerMaterial)
+                float4 _BaseMap_ST;
+                half4 _BaseColor;
+                half4 _SpecColor;
+                half4 _EmissionColor;
+                half _Cutoff;
+                half _Surface;
+            CBUFFER_END
+            
+
+            TEXTURE2D(_SpecGlossMap);       SAMPLER(sampler_SpecGlossMap);
+
+            half4 SampleSpecularSmoothness(float2 uv, half alpha, half4 specColor, TEXTURE2D_PARAM(specMap, sampler_specMap))
+            {
+                half4 specularSmoothness = half4(0, 0, 0, 1);
+            #ifdef _SPECGLOSSMAP
+                specularSmoothness = SAMPLE_TEXTURE2D(specMap, sampler_specMap, uv) * specColor;
+            #elif defined(_SPECULAR_COLOR)
+                specularSmoothness = specColor;
+            #endif
+
+            #ifdef _GLOSSINESS_FROM_BASE_ALPHA
+                specularSmoothness.a = alpha;
+            #endif
+
+                return specularSmoothness;
+            }
+
+            inline void InitializeSimpleLitSurfaceData(float2 uv, out SurfaceData outSurfaceData)
+            {
+                outSurfaceData = (SurfaceData)0;
+
+                half4 albedoAlpha = SampleAlbedoAlpha(uv, TEXTURE2D_ARGS(_BaseMap, sampler_BaseMap));
+                outSurfaceData.alpha = albedoAlpha.a * _BaseColor.a;
+                outSurfaceData.alpha = AlphaDiscard(outSurfaceData.alpha, _Cutoff);
+
+                outSurfaceData.albedo = albedoAlpha.rgb * _BaseColor.rgb;
+                outSurfaceData.albedo = AlphaModulate(outSurfaceData.albedo, outSurfaceData.alpha);
+
+                half4 specularSmoothness = SampleSpecularSmoothness(uv, outSurfaceData.alpha, _SpecColor, TEXTURE2D_ARGS(_SpecGlossMap, sampler_SpecGlossMap));
+                outSurfaceData.metallic = 0.0; // unused
+                outSurfaceData.specular = specularSmoothness.rgb;
+                outSurfaceData.smoothness = specularSmoothness.a;
+                outSurfaceData.normalTS = SampleNormal(uv, TEXTURE2D_ARGS(_BumpMap, sampler_BumpMap));
+                outSurfaceData.occlusion = 1.0;
+                outSurfaceData.emission = SampleEmission(uv, _EmissionColor.rgb, TEXTURE2D_ARGS(_EmissionMap, sampler_EmissionMap));
+            }
+
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+            #if defined(LOD_FADE_CROSSFADE)
+                #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/LODCrossFade.hlsl"
+            #endif
+
+            struct Attributes
+            {
+                float4 positionOS    : POSITION;
+                float3 normalOS      : NORMAL;
+                float4 tangentOS     : TANGENT;
+                float2 texcoord      : TEXCOORD0;
+                float2 staticLightmapUV    : TEXCOORD1;
+                float2 dynamicLightmapUV    : TEXCOORD2;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct Varyings
+            {
+                float2 uv                       : TEXCOORD0;
+
+                float3 positionWS                  : TEXCOORD1;    // xyz: posWS
+
+                #ifdef _NORMALMAP
+                    half4 normalWS                 : TEXCOORD2;    // xyz: normal, w: viewDir.x
+                    half4 tangentWS                : TEXCOORD3;    // xyz: tangent, w: viewDir.y
+                    half4 bitangentWS              : TEXCOORD4;    // xyz: bitangent, w: viewDir.z
+                #else
+                    half3  normalWS                : TEXCOORD2;
+                #endif
+
+                #ifdef _ADDITIONAL_LIGHTS_VERTEX
+                    half4 fogFactorAndVertexLight  : TEXCOORD5; // x: fogFactor, yzw: vertex light
+                #else
+                    half  fogFactor                 : TEXCOORD5;
+                #endif
+
+                #if defined(REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR)
+                    float4 shadowCoord             : TEXCOORD6;
+                #endif
+
+                DECLARE_LIGHTMAP_OR_SH(staticLightmapUV, vertexSH, 7);
+
+            #ifdef DYNAMICLIGHTMAP_ON
+                float2  dynamicLightmapUV : TEXCOORD8; // Dynamic lightmap UVs
+            #endif
+
+                float4 positionCS                  : SV_POSITION;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+                UNITY_VERTEX_OUTPUT_STEREO
+            };
+
+            void InitializeInputData(Varyings input, half3 normalTS, out InputData inputData)
+            {
+                inputData = (InputData)0;
+
+                inputData.positionWS = input.positionWS;
+
+                #ifdef _NORMALMAP
+                    half3 viewDirWS = half3(input.normalWS.w, input.tangentWS.w, input.bitangentWS.w);
+                    inputData.tangentToWorld = half3x3(input.tangentWS.xyz, input.bitangentWS.xyz, input.normalWS.xyz);
+                    inputData.normalWS = TransformTangentToWorld(normalTS, inputData.tangentToWorld);
+                #else
+                    half3 viewDirWS = GetWorldSpaceNormalizeViewDir(inputData.positionWS);
+                    inputData.normalWS = input.normalWS;
+                #endif
+
+                inputData.normalWS = NormalizeNormalPerPixel(inputData.normalWS);
+                viewDirWS = SafeNormalize(viewDirWS);
+
+                inputData.viewDirectionWS = viewDirWS;
+
+                #if defined(REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR)
+                    inputData.shadowCoord = input.shadowCoord;
+                #elif defined(MAIN_LIGHT_CALCULATE_SHADOWS)
+                    inputData.shadowCoord = TransformWorldToShadowCoord(inputData.positionWS);
+                #else
+                    inputData.shadowCoord = float4(0, 0, 0, 0);
+                #endif
+
+                #ifdef _ADDITIONAL_LIGHTS_VERTEX
+                    inputData.fogCoord = InitializeInputDataFog(float4(inputData.positionWS, 1.0), input.fogFactorAndVertexLight.x);
+                    inputData.vertexLighting = input.fogFactorAndVertexLight.yzw;
+                #else
+                    inputData.fogCoord = InitializeInputDataFog(float4(inputData.positionWS, 1.0), input.fogFactor);
+                    inputData.vertexLighting = half3(0, 0, 0);
+                #endif
+
+            #if defined(DYNAMICLIGHTMAP_ON)
+                inputData.bakedGI = SAMPLE_GI(input.staticLightmapUV, input.dynamicLightmapUV, input.vertexSH, inputData.normalWS);
+            #else
+                inputData.bakedGI = SAMPLE_GI(input.staticLightmapUV, input.vertexSH, inputData.normalWS);
+            #endif
+
+                inputData.normalizedScreenSpaceUV = GetNormalizedScreenSpaceUV(input.positionCS);
+                inputData.shadowMask = SAMPLE_SHADOWMASK(input.staticLightmapUV);
+
+                #if defined(DEBUG_DISPLAY)
+                #if defined(DYNAMICLIGHTMAP_ON)
+                inputData.dynamicLightmapUV = input.dynamicLightmapUV.xy;
+                #endif
+                #if defined(LIGHTMAP_ON)
+                inputData.staticLightmapUV = input.staticLightmapUV;
+                #else
+                inputData.vertexSH = input.vertexSH;
+                #endif
+                #endif
+            }
+
+            ///////////////////////////////////////////////////////////////////////////////
+            //                  Vertex and Fragment functions                            //
+            ///////////////////////////////////////////////////////////////////////////////
+
+            // Used in Standard (Simple Lighting) shader
+            Varyings LitPassVertexSimple(Attributes input)
+            {
+                Varyings output = (Varyings)0;
+
+                UNITY_SETUP_INSTANCE_ID(input);
+                UNITY_TRANSFER_INSTANCE_ID(input, output);
+                UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
+
+                VertexPositionInputs vertexInput = GetVertexPositionInputs(input.positionOS.xyz);
+                VertexNormalInputs normalInput = GetVertexNormalInputs(input.normalOS, input.tangentOS);
+
+            #if defined(_FOG_FRAGMENT)
+                    half fogFactor = 0;
+            #else
+                    half fogFactor = ComputeFogFactor(vertexInput.positionCS.z);
+            #endif
+
+                output.uv = TRANSFORM_TEX(input.texcoord, _BaseMap);
+                output.positionWS.xyz = vertexInput.positionWS;
+                output.positionCS = vertexInput.positionCS;
+
+            #ifdef _NORMALMAP
+                half3 viewDirWS = GetWorldSpaceViewDir(vertexInput.positionWS);
+                output.normalWS = half4(normalInput.normalWS, viewDirWS.x);
+                output.tangentWS = half4(normalInput.tangentWS, viewDirWS.y);
+                output.bitangentWS = half4(normalInput.bitangentWS, viewDirWS.z);
+            #else
+                output.normalWS = NormalizeNormalPerVertex(normalInput.normalWS);
+            #endif
+
+                OUTPUT_LIGHTMAP_UV(input.staticLightmapUV, unity_LightmapST, output.staticLightmapUV);
+            #ifdef DYNAMICLIGHTMAP_ON
+                output.dynamicLightmapUV = input.dynamicLightmapUV.xy * unity_DynamicLightmapST.xy + unity_DynamicLightmapST.zw;
+            #endif
+                OUTPUT_SH(output.normalWS.xyz, output.vertexSH);
+
+                #ifdef _ADDITIONAL_LIGHTS_VERTEX
+                    half3 vertexLight = VertexLighting(vertexInput.positionWS, normalInput.normalWS);
+                    output.fogFactorAndVertexLight = half4(fogFactor, vertexLight);
+                #else
+                    output.fogFactor = fogFactor;
+                #endif
+
+                #if defined(REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR)
+                    output.shadowCoord = GetShadowCoord(vertexInput);
+                #endif
+
+                return output;
+            }
+
+            half3 CalculateHair(Light light, InputData inputData, SurfaceData surfaceData)
+            {
+                half3 attenuatedLightColor = light.color * (light.distanceAttenuation * light.shadowAttenuation);
+                half3 lightDiffuseColor = LightingLambert(attenuatedLightColor, light.direction, inputData.normalWS);
+          
+            
+                half3 lightSpecularColor = half3(0,0,0);
+                #if defined(_SPECGLOSSMAP) || defined(_SPECULAR_COLOR)
+                half smoothness = exp2(10 * surfaceData.smoothness + 1);
+            
+                lightSpecularColor += LightingSpecular(attenuatedLightColor, light.direction, inputData.normalWS, inputData.viewDirectionWS, half4(surfaceData.specular, 1), smoothness);
+                #endif
+            
+            #if _ALPHAPREMULTIPLY_ON
+                return lightDiffuseColor * surfaceData.albedo * surfaceData.alpha + lightSpecularColor;
+            #else
+                return lightDiffuseColor * surfaceData.albedo + lightSpecularColor;
+            #endif
+            }
+
+            ////////////////////////////////////////////////////////////////////////////////
+            /// Phong lighting...
+            ////////////////////////////////////////////////////////////////////////////////
+            half4 UniversalFragmentHair(InputData inputData, SurfaceData surfaceData)
+            {
+                #if defined(DEBUG_DISPLAY)
+                half4 debugColor;
+
+                if (CanDebugOverrideOutputColor(inputData, surfaceData, debugColor))
+                {
+                    return debugColor;
+                }
+                #endif
+
+                uint meshRenderingLayers = GetMeshRenderingLayer();
+                half4 shadowMask = CalculateShadowMask(inputData);
+                AmbientOcclusionFactor aoFactor = CreateAmbientOcclusionFactor(inputData, surfaceData);
+                Light mainLight = GetMainLight(inputData, shadowMask, aoFactor);
+
+                MixRealtimeAndBakedGI(mainLight, inputData.normalWS, inputData.bakedGI, aoFactor);
+
+                inputData.bakedGI *= surfaceData.albedo;
+
+                // LightingData lightingData = CreateLightingData(inputData, surfaceData);
+                LightingData lightingData = (LightingData)0;
+            #ifdef _LIGHT_LAYERS
+                if (IsMatchingLightLayer(mainLight.layerMask, meshRenderingLayers))
+            #endif
+                {
+                    lightingData.mainLightColor += CalculateHair(mainLight, inputData, surfaceData);
+                }
+
+                #if defined(_ADDITIONAL_LIGHTS)
+                uint pixelLightCount = GetAdditionalLightsCount();
+
+                #if USE_FORWARD_PLUS
+                for (uint lightIndex = 0; lightIndex < min(URP_FP_DIRECTIONAL_LIGHTS_COUNT, MAX_VISIBLE_LIGHTS); lightIndex++)
+                {
+                    FORWARD_PLUS_SUBTRACTIVE_LIGHT_CHECK
+
+                    Light light = GetAdditionalLight(lightIndex, inputData, shadowMask, aoFactor);
+            #ifdef _LIGHT_LAYERS
+                    if (IsMatchingLightLayer(light.layerMask, meshRenderingLayers))
+            #endif
+                    {
+                        lightingData.additionalLightsColor += CalculateHair(light, inputData, surfaceData);
+                    }
+                }
+                #endif
+
+                LIGHT_LOOP_BEGIN(pixelLightCount)
+                    Light light = GetAdditionalLight(lightIndex, inputData, shadowMask, aoFactor);
+            #ifdef _LIGHT_LAYERS
+                    if (IsMatchingLightLayer(light.layerMask, meshRenderingLayers))
+            #endif
+                    {
+                        lightingData.additionalLightsColor += CalculateHair(light, inputData, surfaceData);
+                    }
+                LIGHT_LOOP_END
+                #endif
+
+                #if defined(_ADDITIONAL_LIGHTS_VERTEX)
+                lightingData.vertexLightingColor += inputData.vertexLighting * surfaceData.albedo;
+                #endif
+
+                return CalculateFinalColor(lightingData, surfaceData.alpha);
+            }
+
+            // Used for StandardSimpleLighting shader
+            void LitPassFragmentSimple(
+                Varyings input
+                , out half4 outColor : SV_Target0
+            #ifdef _WRITE_RENDERING_LAYERS
+                , out float4 outRenderingLayers : SV_Target1
+            #endif
+            )
+            {
+                UNITY_SETUP_INSTANCE_ID(input);
+                UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+
+                SurfaceData surfaceData;
+                InitializeSimpleLitSurfaceData(input.uv, surfaceData);
+
+            #ifdef LOD_FADE_CROSSFADE
+                LODFadeCrossFade(input.positionCS);
+            #endif
+
+                InputData inputData;
+                InitializeInputData(input, surfaceData.normalTS, inputData);
+                SETUP_DEBUG_TEXTURE_DATA(inputData, input.uv, _BaseMap);
+
+            #ifdef _DBUFFER
+                ApplyDecalToSurfaceData(input.positionCS, surfaceData, inputData);
+            #endif
+
+                half4 color = UniversalFragmentHair(inputData, surfaceData);
+                color.rgb = MixFog(color.rgb, inputData.fogCoord);
+                // color.a = OutputAlpha(color.a, IsSurfaceTypeTransparent(_Surface));
+                // color.a = OutputAlpha(color.a, IsSurfaceTypeTransparent(_Surface));
+
+
+                outColor = color;
+
+            #ifdef _WRITE_RENDERING_LAYERS
+                uint renderingLayers = GetMeshRenderingLayer();
+                outRenderingLayers = float4(EncodeMeshRenderingLayer(renderingLayers), 0, 0, 0);
+            #endif
+            }
+
+
+#endif
